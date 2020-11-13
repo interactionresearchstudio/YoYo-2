cf176ec89a586275b19425982ee5247548ae83a8
andysheen <andymsheen@gmail.com>
2020-11-13 15:27:51 +0000

    updated sections to 7

--- servo.ino
+++ servo.ino
@@ -4,39 +4,41 @@ void setupServo() {
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
-    servo.setPeriodHertz(50);    // standard 50 hz servo
-    servo.attach(servoPin, 150, 2500); // attaches the servo on pin 18 to the servo object
-    servo.write(0);
+  servo.setPeriodHertz(50);    // standard 50 hz servo
+  servo.attach(servoPin, 150, 2500); // attaches the servo on pin 18 to the servo object
+  servo.write(0);
 
 }
 
-void setAngle(byte angle){
+void setAngle(byte angle) {
   servo.write(angle);
   currentAngle = angle;
 }
 
-byte getAngle(){
+byte getAngle() {
   return currentAngle;
 }
 
-void generateAngles(){
-  for(byte i = 0; i < numOfSections; i++){
-    angles[i] = (i*(180/(numOfSections)))+((180/numOfSections)/2);
+void generateAngles() {
+  for (byte i = 0; i < numOfSections; i++) {
+    angles[i] = (i * (180 / (numOfSections))) + ((180 / numOfSections) / 2);
   }
+  angles[0] = angles[0] - 2;
+  angles[numOfSections-1] = angles[numOfSections-1] + 2;
 }
 
 void advanceAngle() {
   // Advance
-   if (isIncreasing) currAngleArrayPosition++;
+  if (isIncreasing) currAngleArrayPosition++;
   else currAngleArrayPosition--;
-  
+
   // Change direction if we're at either end
   if (currAngleArrayPosition == numOfSections) {
     isIncreasing = false;
-    currAngleArrayPosition = currAngleArrayPosition-2 ;
+    currAngleArrayPosition = currAngleArrayPosition - 2 ;
   } else if (currAngleArrayPosition < 0) {
     isIncreasing = true;
-    currAngleArrayPosition = currAngleArrayPosition+2;
+    currAngleArrayPosition = currAngleArrayPosition + 2;
   }
 
   currentAngle = angles[currAngleArrayPosition];
